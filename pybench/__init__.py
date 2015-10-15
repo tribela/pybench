97d06b4aa2643b40890e9a5d229b80005c8521cd
kjwon15 <kjwonmail@gmail.com>
2015-10-15 16:21:00 +0900

    Send packet to a or b with ratio

--- pybench/__init__.py
+++ pybench/__init__.py
@@ -1,15 +1,25 @@
+from __future__ import division
 import multiprocessing
 import time
 from pybench.fakeswitch import FakeSwitch
 from pybench.util import make_random_mac, make_random_ip
 
 
-def benchmark_switch(switch, queue, duration):
+def benchmark_switch(switch, queue, duration, dst1, dst2, ratio):
     dstmac = make_random_mac()
-    dstip = '192.168.0.2'
+
+    ratio /= 100
+    ratio_order = 0
 
     end_time = time.time() + duration
     while time.time() < end_time:
+        ratio_order += ratio
+        if ratio_order >= 1:
+            dstip = dst1
+            ratio_order %= 1
+        else:
+            dstip = dst2
+
         switch.send_packet_in(
             srcmac=make_random_mac(), dstmac=dstmac,
             srcip=make_random_ip(), dstip=dstip)
@@ -19,12 +29,13 @@ def benchmark_switch(switch, queue, duration):
     queue.put(switch.get_packet_count())
 
 
-def benchmark(host, port, num_of_switches, duration):
+def benchmark(host, port, num_of_switches, duration, dst1, dst2, ratio):
     queue = multiprocessing.Queue()
     switches = [FakeSwitch(host, port, dpid)
                 for dpid in range(num_of_switches)]
     processes = [multiprocessing.Process(
-        target=benchmark_switch, args=(switch, queue, duration))
+        target=benchmark_switch,
+        args=(switch, queue, duration, dst1, dst2, ratio))
         for switch in switches]
 
     for switch in switches:
--- pybench/__main__.py
+++ pybench/__main__.py
@@ -11,10 +11,17 @@ arg_parser.add_argument('-n', '--switch-num', type=int, default=1,
                         help='Number of switches')
 arg_parser.add_argument('-t', '--duration', type=int, default=10,
                         help='Test duration')
+arg_parser.add_argument('-d', '--dstip', nargs=2,
+                        help='Destination ips')
+arg_parser.add_argument('-r', '--ratio', type=int, default=50,
+                        help='dst1:all ratio in percentage')
 
 def main():
     args = arg_parser.parse_args()
-    benchmark(args.host, args.port, args.switch_num, args.duration)
+    benchmark(args.host, args.port,
+              args.switch_num, args.duration,
+              args.dstip[0], args.dstip[1], args.ratio)
+
 
 if __name__ == '__main__':
     main()
